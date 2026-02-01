7cd1183e4c455d7bafacd8a3435dd398c1c92e70
TeddyDore11780 <150923996+TeddyDore11780@users.noreply.github.com>
2026-02-01 10:41:38 +0200

    Update Streamlit app title and navigation menu

--- run_streamlit.py
+++ run_streamlit.py
@@ -2,24 +2,25 @@ import streamlit as st
 import app
 import app_plots
 import app_profiler
+import app_profiler_menus
 
 st.set_page_config(
-    page_title="CSS2026 Bike App",
-    layout="wide"
+    page_title="CSS2026 Streamlit App",
+    layout="wide",
 )
 
-st.title("🚲 CSS2026 Bike Rentals Dashboard")
+st.title("CSS2026 Dashboard")
 
 menu = st.sidebar.radio(
-    "Navigation",
-    ["Overview", "Plots", "Profiler"]
+    "Main Navigation",
+    ["Overview", "Plots", "Profiler", "Researcher Menu"],
 )
 
 if menu == "Overview":
     app.main()
-
 elif menu == "Plots":
     app_plots.main()
-
 elif menu == "Profiler":
     app_profiler.main()
+elif menu == "Researcher Menu":
+    app_profiler_menus.main()
