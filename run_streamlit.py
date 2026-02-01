cd4825127325a3446fac2c0536f6552cd41a4642
TeddyDore11780 <150923996+TeddyDore11780@users.noreply.github.com>
2026-02-01 10:34:49 +0200

    Refactor run_streamlit.py for improved navigation

--- run_streamlit.py
+++ run_streamlit.py
@@ -1,17 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
+import streamlit as st
+import app
+import app_plots
+import app_profiler
 
-@author: ST Mbelebele 208015035
-"""
-
-import subprocess
-
-file = "app.py"
-file = "app_plots.py"
-file = "app_profiler.py"
-#file = "app_profiler_menus.py"
-
-
-subprocess.Popen(
-    ["streamlit", "run", file], shell=True
+st.set_page_config(
+    page_title="CSS2026 Bike App",
+    layout="wide"
 )
+
+st.title("🚲 CSS2026 Bike Rentals Dashboard")
+
+menu = st.sidebar.radio(
+    "Navigation",
+    ["Overview", "Plots", "Profiler"]
+)
+
+if menu == "Overview":
+    app.main()
+
+elif menu == "Plots":
+    app_plots.main()
+
+elif menu == "Profiler":
+    app_profiler.main()
