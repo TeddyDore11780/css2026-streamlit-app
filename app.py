9c9ed5a7d117a7f597bbcbf89ef5ccd03fbd7f79
TeddyDore11780 <150923996+TeddyDore11780@users.noreply.github.com>
2026-02-01 11:21:43 +0200

    Refactor app.py and update author information

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 27 17:32:56 2026
 
-@author: BBarsch
+
+@author: Sonwabile Theodore Mbelebele 208015035
 """
 
 import streamlit as st
 import pandas as pd
 
 def main():
-    st.header("Overview")
+    st.title("Title heading")
 
+    st.write("Hello, Streamlit!")
+    st.write("Hello2")
+
+    st.header("Overview")
     df = pd.read_csv("bike_data.csv")
     st.dataframe(df.head())
 
-st.write("Hello2")
-
-st.title("Title heading")
-
-st.write("Hello, Streamlit!")
-
-st.header("Number selection")
-
-number = st.slider("Pick a number", 1, 100)
-
-st.write(f"You picked: {number}")
+    st.header("Number selection")
+    number = st.slider("Pick a number", 1, 100)
+    st.write(f"You picked: {number}")
