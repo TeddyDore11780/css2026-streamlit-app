8df99a1a65e4f63bd5ee4389f84bb6add5685a49
TeddyDore11780 <150923996+TeddyDore11780@users.noreply.github.com>
2026-02-01 10:49:03 +0200

    Refactor Streamlit app structure and navigation

--- run_streamlit.py
+++ run_streamlit.py
@@ -2,15 +2,28 @@
 
 @author: Sonwabile Theodore Mbelebele 208015035
 """
+import streamlit as st
+import app
+import app_plots
+import app_profiler
 
-import subprocess
-
-file = "app.py"
-file = "app_plots.py"
-file = "app_profiler.py"
-#file = "app_profiler_menus.py"
-
-
-subprocess.Popen(
-    ["streamlit", "run", file], shell=True
+st.set_page_config(
+    page_title="CSS2026 Bike App",
+    layout="wide"
 )
+
+st.title("🚲 CSS2026 Bike Rentals Dashboard")
+
+menu = st.sidebar.radio(
+    "Navigation",
+    ["Overview", "Plots", "Profiler"]
+)
+
+if menu == "Overview":
+    app.main()
+
+elif menu == "Plots":
+    app_plots.main()
+
+elif menu == "Profiler":
+    app_profiler.main()
